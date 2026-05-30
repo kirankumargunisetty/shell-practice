43c73c91fc4c2bf9f65b5b13cf0eca45d00bc320
kirankumargunisetty <gunisettykirankumar7@gmail.com>
2026-05-30 12:02:13 +0530

    Conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going to blast you!"
+echo "Iran: Yes, Please . You are Welcome"
+echo "Trump: Hey Iran I am Serious"
+echo "Iran: Hey Trump, We are also serious" 
