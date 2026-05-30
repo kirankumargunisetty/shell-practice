f2d43158ffc1a38f6d66daa95103d0c443a1ce72
kirankumargunisetty <gunisettykirankumar7@gmail.com>
2026-05-30 12:09:40 +0530

    Conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Trump: Hey Iran, we are going to blast you!"
-echo "Iran: Yes, Please . You are Welcome"
-echo "Trump: Hey Iran I am Serious"
-echo "Iran: Hey Trump, We are also serious" 
+PERSON1=Trump
+PERSON2=Iran
+# there wont be any space in = if you compare above lines there is no space before or after
+
+echo "$PERSON1: Hey $Iran, we are going to blast you!"
+# if the anything starts from $ shell consider it as Variable
+echo "$Iran: Yes, Please . You are Welcome"
+echo "$PERSON1: Hey $Iran I am Serious"
+echo "$Iran: Hey $PERSON1, We are also serious" 
