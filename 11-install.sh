e6017844f3da0e9f3737f5047e4d023369bc25ac
kirankumargunisetty <gunisettykirankumar7@gmail.com>
2026-05-31 18:57:14 +0530

    variables

--- 11-install.sh
+++ 11-install.sh
@@ -5,7 +5,17 @@ USERID=$(id -u)
 #check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with sudo access"
-    exit 1
+    exit 1 # if we comment this line code wont break it will print the i am continuing line and then exit
 fi
 
-echo "I am continuing..."
+#echo "I am continuing..."
+
+echo "Installing Postgresql"
+dnf install postgresql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Postgresql is ... Failed"
+    exit 1
+else
+    echo installing Postgresql is ... SUCCESS
+fi
