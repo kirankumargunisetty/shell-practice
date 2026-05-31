0dbec792c6b8b22ff5110098cb58f9a7dbeb0974
kirankumargunisetty <gunisettykirankumar7@gmail.com>
2026-05-31 17:28:50 +0530

    variables

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NUMB=10
+NUM1=10
 #NUM2=20
 NUM2=linux
-# if we give linux in place of number i.e 10 shell wont throw any error while doing sum instead it will ignore in
+# if we give linux in place of number i.e 20 shell wont throw any error while doing sum instead it will ignore in
 # doing sum and give's only sum value as 10
 
 SUM=$(($NUM1+$NUM2))
